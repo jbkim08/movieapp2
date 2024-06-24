8e6ff21c95988d27f8c565ed8f0b0a453d2303c7
JBKIM <drv98@naver.com>
2024-06-24 15:03:44 +0900

    최초커밋

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+
+      <main>메인 컨텐트</main>
+    </div>
+  );
+}
+
+export default App;
